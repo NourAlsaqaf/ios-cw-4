c84cc17a45f4d40e107f18e3ac4f54d737b270f6
NourAlsaqaf <100691291+NourAlsaqaf@users.noreply.github.com>
2022-03-05 13:44:59 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var name = "نور"
+var number = 2
+var hobby = " العزف والحياكة والأعمال اليدوية والطبخ وتعديل الفيديوهات والصور"
+print("السلام عليكم معاكم \(name)")
+print("رقمي المفضل هو \(number)")
+print("أحب\(hobby )")
